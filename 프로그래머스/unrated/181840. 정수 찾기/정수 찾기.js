7d8b9f80a5c7838a535490a264aa0124d3f6b23e
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2023-11-26 23:34:26 +0900

    [level unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -0,0 +1,11 @@
+function solution(num_list, n) {
+    var answer = 0;
+    if(num_list.includes(n)){
+        answer =1;
+    }
+    return answer;
+}
+
+function solution(num_list, n) {
+    return num_list.includes(n) ? 1 : 0;
+}
