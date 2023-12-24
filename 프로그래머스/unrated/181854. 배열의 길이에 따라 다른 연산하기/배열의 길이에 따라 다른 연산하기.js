cab750a490bdebc07ce2f08cb3c6a936f79ba220
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2023-12-24 22:41:00 +0900

    [level unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,25 +1,18 @@
 function solution(arr, n) {
     let answer = [];
+    let result = [...answer];
     
-    // 홀수라면(길이)
     if(arr.length % 2 == 1){
       for (let i = 0; i < arr.length; i++) {
-          // 인덱스 짝수
-          if(i % 2 == 0){
-            answer.push(arr[i] + n);
-          }else{
-            answer.push(arr[i]);
-          }
+          value = i % 2 == 0 ? arr[i] + n : arr[i];
+          result.push(value);
         }
     }else{
       for (let i = 0; i < arr.length; i++){
-          if(i % 2 == 1){
-            answer.push(arr[i] + n);
-          }else{
-            answer.push(arr[i]); 
-          }
+          value = i % 2 == 1 ? arr[i] + n : arr[i];
+          result.push(value);
       }  
     }
-    return answer;
+    return result;
 
 }
