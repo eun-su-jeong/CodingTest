7277e0bef0e5a333aea3920916e57025f83e8e3f
eunjjang <dmswid0@naver.com>
2024-06-06 08:47:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.09 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -6,24 +6,19 @@ function solution(numbers, target) {
 }
 function dfs (numbers, target, sum, idx){
     if(numbers.length === idx){
+        
         if(target === sum){
             count++;
         }
         return;
     }
-    dfs(numbers, target, sum + numbers[idx], idx+1);
-    dfs(numbers, target, sum - numbers[idx], idx+1);
+    dfs(numbers, target, sum + numbers[idx], idx + 1);
+    dfs(numbers, target, sum - numbers[idx], idx + 1);
 }
 
-
-
-
-// 탈출조건
-
-// DFS
-
-// 배열, 타겟(숫자결과값), numbers.length, index 경우의 수 구하기 sum == target
-
-// 생각의 회로가 어뜨케 되나염????????
-
-
+// 0. 어떤 알고리즘 문제 인지 dfs -> 재귀 함수
+// 1. 필요한 매개변수 
+// -> (숫자가 담긴 배열, 주어진 숫자, 적절히 더하거나 빼거나, 배열이니까 인덱스)
+// -> (numbers, target, sum, idx)
+// 2. 구해야 되는 값 : 타겟 넘버를 만드는 방법의 수 
+// 3. 탈출 조건 : 흠.. 바로 생각이 안나네ㅜ 계속 더할 수 없으니까... 흠.. 
