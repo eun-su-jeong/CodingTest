a051dca868939c90285e78b9bf4b436b9e72ec79
eunjjang <dmswid0@naver.com>
2024-06-07 11:00:01 +0900

    [level 2] Title: 타겟 넘버, Time: 1.29 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,16 @@
 let count = 0;
 
 function solution(numbers, target) {
-    dfs(numbers, target, 0, 0);
+   dfs(numbers, target, 0, 0);
     return count;
-    
 }
 function dfs (numbers, target, sum, idx){
     if(numbers.length === idx){
-        if(sum === target){
+        if(target === sum){
             count++;
         }
         return;
     }
-    dfs(numbers, target, sum + numbers[idx], idx +1);
-    dfs(numbers, target, sum - numbers[idx], idx +1);
+    dfs(numbers, target, sum + numbers[idx], idx + 1);
+    dfs(numbers, target, sum - numbers[idx], idx + 1);
 }
-
