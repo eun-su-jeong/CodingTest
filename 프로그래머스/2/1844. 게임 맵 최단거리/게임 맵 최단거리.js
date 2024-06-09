06c0d7d9bdc5282f5beb4310bb30ee81b3deb612
eunjjang <dmswid0@naver.com>
2024-06-09 16:05:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.81 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -28,3 +28,15 @@ function solution(maps) {
     
     return -1;
 }
+
+// **문제 이해**
+// 최솟값 BFS => visited , queue : shfit  or stack : pop 
+// 캐릭터가 갈 수 있는 길 
+// => 동서남북 1칸씩, 벽 X == 0, 길o == 1, 맵에서 벗어난 길x, 방문했던 곳 x, 상대팀 진영에 도착 할 수 x == -1
+// maps n * m 이차원 배열
+
+// ** 셋팅 값 **
+// map의 x, y 길이값을 n, m 이라고 
+// visited 초기화 => 이건 어려워서 그냥 외워야 할 듯 
+// queue [위치좌표x, 위치좌표y, count]
+// x, y 각각 동서남북 위치
