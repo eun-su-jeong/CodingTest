0964269f420d2a287dbea82a03d0d800315bfde2
eunjjang <dmswid0@naver.com>
2024-10-22 18:44:42 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    let resultOdd = 0;
+    let resultEven = 0;
+    
+    for(let i = 0; i < num_list.length; i++){
+        if(i % 2 == 0) {
+            resultEven += num_list[i];
+        }
+        else{
+            resultOdd += num_list[i];
+        }
+    }
+    
+    return Math.max(resultOdd, resultEven);
+}
