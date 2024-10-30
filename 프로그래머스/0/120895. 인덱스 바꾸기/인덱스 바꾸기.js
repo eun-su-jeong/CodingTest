9bb35d2a1df40135a0fd82e16ad2633c0956f56e
eunjjang <dmswid0@naver.com>
2024-10-30 21:10:42 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,16 +1,6 @@
 function solution(my_string, num1, num2) {
-    let answer = [];
-    for(let i = 0; i < my_string.length; i++){
-        if(num1 === i){
-            answer += my_string[num2];
-        }else if(num2 === i){
-            answer += my_string[num1];
-        }else{
-            answer += my_string[i];
-        }
-    }
-    return answer;
+    let arr = [];
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
 }
-// num1과 num2의 값을 구한다 
-// my_string을 for문을 돌린다
-// my_string[i] 와 같은 값? 위치를 반대로 지정해 출력한다
