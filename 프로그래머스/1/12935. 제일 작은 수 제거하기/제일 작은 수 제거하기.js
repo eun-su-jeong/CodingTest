4258b84dbbf916722ffe95f8564d421bcec97e98
eunjjang <dmswid0@naver.com>
2024-10-30 22:47:35 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 25.47 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -2,14 +2,19 @@ function solution(arr) {
     // 배열중에 가장 작은 수 찾기 
     let answer = [];
     // 최소값 찾기
-    let minNum = arr[0];
-    let idx = 0;
-    for(let i = 1; i < arr.length; i++){
-        if(minNum > arr[i]) {
-            minNum = arr[i];
-            idx = i;
-        }
-    }
+    // for(let i = 1; i < arr.length; i++){
+    //     if(minNum > arr[i]) {
+    //         minNum = arr[i];
+    //         idx = i;
+    //     }
+    // }
+    let sortArr = [...arr];
+    sortArr.sort((a , b) => a - b);
+    minNum = sortArr.shift();
+    console.log(sortArr);
+    console.log(arr);
+    
+
     
     // // 최소값이 아닌 값들을 answer 리스트에 담기
     // for(let j = 0; j < arr.length; j++){
@@ -18,10 +23,10 @@ function solution(arr) {
     //     }
     // }
     
-    // answer = arr.filter(num => num !== minNum);
+    answer = arr.filter(num => num !== minNum);
 
-    answer = [...arr];
-    answer.splice(idx, 1);
+    // answer = [...arr];
+    // answer.splice(idx, 1);
     // 아무것도 안 담겼을 때
     if(answer.length <= 1) {
         return [-1];
