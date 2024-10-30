d1e1d1a348ed3c029a472220a72b94a1c1d01380
eunjjang <dmswid0@naver.com>
2024-10-30 22:37:25 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 6.37 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,25 @@
+function solution(arr) {
+    // 배열중에 가장 작은 수 찾기 
+    let answer = [];
+    // 최소값 찾기
+    let minNum = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        if(minNum > arr[i]) {
+            minNum = arr[i];
+        }
+    }
+    
+    // 최소값이 아닌 값들을 answer 리스트에 담기
+    for(let j = 0; j < arr.length; j++){
+        if (arr[j] != minNum) {
+            answer.push(arr[j]);    
+        }
+    }
+
+    // 아무것도 안 담겼을 때
+    if(answer.length == 0) {
+        answer.push(-1);
+    }
+    
+    return answer; 
+}
