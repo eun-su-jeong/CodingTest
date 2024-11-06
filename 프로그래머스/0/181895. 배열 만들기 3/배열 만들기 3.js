4395a8e5ade1c0d10734bd65813258ce954d56ea
eunjjang <dmswid0@naver.com>
2024-11-06 22:20:04 +0900

    [level 0] Title: 배열 만들기 3, Time: 7.51 ms, Memory: 42.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,10 @@
+function solution(arr, intervals) {
+    let result = [];
+    for(let i = 0; i < intervals.length; i++){
+        intervals[i];
+        for(let j = intervals[i][0]; j <= intervals[i][1]; j++){
+            result.push(arr[j]);
+        }
+    }
+    return result;
+}
