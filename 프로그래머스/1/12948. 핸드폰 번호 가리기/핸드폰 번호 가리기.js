a87ce2a7319df5fe8fb96d7c16e2c70da51b9eec
eunjjang <dmswid0@naver.com>
2024-11-06 17:10:06 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,10 +1,8 @@
 function solution(phone_number) {
-    let answer = '';
     const number = Array.from(phone_number);
     
     for(let i = 0; i < number.length - 4; i++){
         number[i] = '*';
     }
-  
     return number.join('');
 }
