c91c8efe9cdf2c463b77b630b150046f577a8f38
eunjjang <dmswid0@naver.com>
2024-12-08 00:26:30 +0900

    [Silver V] Title: 셀프 넘버, Time: 152 ms, Memory: 10276 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -1,23 +1,24 @@
-let visited = Array(new Array(10001).fill(false));
+let visited = Array(10001).fill(false);
 let result = '';
 
-for(let i = 1; i <= 10000; i++){
+for(let i = 1; i < 10000; i++){
     if(!visited[i]){
         result += i + '\n';
         dfs(i);
     }
 }
 function dfs(num){
-   visited[num] = true;
-   let nextNum = num;
-   while(num > 0) {
-       nextNum += num % 10;
-       num /= 10;
-       num = Math.floor(num);
-   }
-
-   if (nextNum <= 10000 && !visited[nextNum]) {
-       dfs(nextNum);
-   }
+    if(num > 10000){
+        return;
+    }
+    
+    visited[num] = true;
+    
+    let nextNum = num;
+    while(num > 0){
+        nextNum += num % 10;
+        num = Math.floor(num / 10);
+    }
+    dfs(nextNum);
 }
 console.log(result);
