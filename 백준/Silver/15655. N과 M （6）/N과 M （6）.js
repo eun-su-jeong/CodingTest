39a5c95c76abb4edf4ae8dd52aa09a5c342af09c
eunjjang <dmswid0@naver.com>
2024-12-08 22:16:26 +0900

    [Silver III] Title: N과 M (6), Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+// input.txt 파일에서 입력값을 읽음
+const input =fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// 첫 번째 줄에서 N과 M 추출
+const [N, M] = input[0].split(' ').map(Number);
+
+// 두 번째 줄에서 카드 배열 추출
+const numbers = input[1].split(' ').map(Number).sort((a, b) => a - b);
+
+let arr = [];
+let result = [];
+
+dfs(0, 0);
+function dfs(index, start){
+    if(index === M){
+        result.push(arr.join(' '));
+        return;
+    }
+    for(let i = start; i < N; i++){
+            arr[index] = numbers[i];
+            dfs(index + 1, i + 1);
+        }
+}
+console.log(result.join('\n'));
