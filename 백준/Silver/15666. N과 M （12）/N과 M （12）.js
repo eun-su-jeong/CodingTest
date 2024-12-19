95b3b81197921408ee69abb8c67dc2fe4bf1c4cf
eunjjang <dmswid0@naver.com>
2024-12-19 17:54:17 +0900

    [Silver II] Title: N과 M (12), Time: 284 ms, Memory: 12788 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+// input.txt 파일에서 입력값을 읽음
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// 첫 번째 줄에서 N과 M 추출
+const [N, M] = input[0].split(' ').map(Number);
+const cards = input[1]
+	.split(' ')
+	.map(Number)
+	.sort((a, b) => a - b);
+
+let arr = [];
+let result = new Set();
+
+dfs(0, 0);
+function dfs(index, start) {
+	if (index === M) {
+		result.add(arr.join(' '));
+		return;
+	}
+	for (let i = start; i < N; i++) {
+		arr[index] = cards[i];
+		dfs(index + 1, i);
+	}
+}
+for (let item of result) {
+	console.log(item);
+}
