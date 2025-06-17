b1cf5c4fb41cf4831a08873d28019d57bccf6ae4
eunjjang <dmswid0@naver.com>
2025-06-17 22:07:20 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,12 +1,8 @@
-function solution(numer1, denom1, numer2, denom2) {
-    //유클리드 호재법?
-    //최대공약수를 찾아서 
-    //최소공배수 두 수 곱한값 A*B/G(최대공약수)
+function solution(numer1, denom1, numer2, denom2)
+{
+    let num1 = denom1 * denom2;
+    let num2 = numer1 * denom2 + numer2 * denom1
     
-    let num1 = denom1 * denom2 // 분모
-    let num2 = numer1 * denom2 + numer2 * denom1 // 분자
-    
-    // 최대 공약수 구하기
     function gcd(a, b){
         if(b === 0){
             return a;
@@ -15,13 +11,10 @@ function solution(numer1, denom1, numer2, denom2) {
     }
     
     const g = gcd(num1, num2);
+    
     num1 = num1 / g;
     num2 = num2 / g;
     
-//     let list = [];
-//     list.push(num2);
-//     list.push(num1);
-    
-//     return list;
     return [num2, num1];
+    
 }
